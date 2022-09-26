867255bf888a5432487d0a2d8c8a762d792a0c32
Cristian64037 <clara03@aurora.edu>
2022-09-25 22:39:00 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathProject_Capstone_
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ApiKeys apiKey= new ApiKeys();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
