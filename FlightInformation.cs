7cb68c39014d04f4495af3a28e4a3b414e702e83
Cristian64037 <clara03@aurora.edu>
2022-09-28 15:21:25 -0500

    Update FlightInformation.cs
    rid of extra space

--- FlightInformation.cs
+++ FlightInformation.cs
@@ -2,14 +2,7 @@ namespace MathProject_Capstone_
 {
     public class FlightInformation
     {
-        
-
-        
-
         public string departureCity { get; set; }
-
-        
-
         public string arrivalCity { get; set; }
         public string date { get; set; }
         public FlightInformation(string departureCity,string arrivalCity,string date)
